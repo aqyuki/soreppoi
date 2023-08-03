60c66f96d7881d6f85112065bc7d3ac3887659ba
yksk171118@gmail.com <yksk171118@gmail.com>
2023-08-03 19:24:01 +0900

    :sparkles: Add root command

---
+++ cmd/root.go
@@ -0,0 +1,28 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "soreppoi",
+	Short: "It displays a screen that looks like that.",
+	Long:  `You can use it in front of your friends who are PC novices and be a little proud of yourself!`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
